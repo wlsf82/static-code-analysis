cf5c0b1c021355d378836f0914c37ba1f7134e4b
Walmyr <wlsf82@gmail.com>
2025-08-28 01:52:56 +0200

    Create cypress project

---
+++ README.md
@@ -0,0 +1,3 @@
+# static-code-analysis
+
+A sample project to start a discussion about the importance of static code analysis for testing code.
---
+++ cypress/e2e/spec.cy.js
@@ -0,0 +1,20 @@
+describe('Sauce Demo', () => {
+
+
+  it('successfully logs in', () => {  
+
+
+    cy.visit("/")
+
+    cy.get('[data-test="username"]').type(Cypress.env('SAUCEDEMO_USER'))
+    cy.get('[data-test="password"]').type(Cypress.env('SAUCEDEMO_PWD'), { log: false })
+    cy.get('[data-test="login-button"').click();
+
+
+      cy.url().should('be.equal', `${Cypress.config('baseUrl')}/inventory.html`)
+
+
+  })
+
+
+})
